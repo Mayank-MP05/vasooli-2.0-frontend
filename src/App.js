2418b94e7763aea7c4f58c7406213b5d47e871c1
Mayank-MP05 <mayank5pande@gmail.com>
2022-05-23 08:36:20 +0530

    React routing setup done for dummy rroutes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./redux/reducer/counter";
+import Routes from "./routes";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-  return (
-    <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
