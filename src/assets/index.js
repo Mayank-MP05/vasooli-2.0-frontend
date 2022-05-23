7662aa3ee71aa1989c726498a9af7d3365c0b1dd
Mayank-MP05 <mayank5pande@gmail.com>
2022-05-23 11:44:31 +0530

    Login UI added with no functional feature tested on modal

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import SigninMoneyIconImport from "./img/signin-money-icon.svg";
+import SignupMoneyIconImport from "./img/signup-money-icon.svg";
+export const SigninMoneyIcon = SigninMoneyIconImport;
+export const SignupMoneyIcon = SignupMoneyIconImport;
