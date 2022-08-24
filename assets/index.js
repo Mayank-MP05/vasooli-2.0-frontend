c482d4d566f491a03f309f2723a6c7695202c8fe
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-24 11:55:55 +0530

    Next JS title and Favicon updates #6

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+import SigninMoneyIconImport from "./img/signin-money-icon.svg";
+import SignupMoneyIconImport from "./img/signup-money-icon.svg";
+import NavbarLogoImport from "./img/icon.png";
+
+// Page icon imports
+import TransactionIconImport from "./img/transactions-icon.svg";
+import VasooliIconImport from "./img/vasooli-money-bag-icon.svg";
+import ProfileIconImport from "./img/profile-icon.svg";
+import EditProfileIconImport from "./img/edit-profile-icon.svg";
+
+export const SigninMoneyIcon = SigninMoneyIconImport;
+export const SignupMoneyIcon = SignupMoneyIconImport;
+export const TransactionIcon = TransactionIconImport;
+export const VasooliIcon = VasooliIconImport;
+export const ProfileIcon = ProfileIconImport;
+export const EditProfileIcon = EditProfileIconImport;
+export const NavbarLogo = NavbarLogoImport;
